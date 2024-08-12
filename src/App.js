b54792f47e64b205a95142e7d1b27d2c9c451277
hm.oh <hm.oh@taekwang.com>
2024-08-12 14:40:45 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customer = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길도',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '박찬호',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '선동열',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop manangement system!</h2>
+    <div>
+      {
+        customer.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
